d3a81c6c587712a8e5e3e48fe630ca01ceb7a889
Naresh <naresh.khatri2345@gmail.com>
2020-06-23 21:40:04 +0530

    updated again

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js test-project "/>
+    <HelloWorld msg="Welcome to Your Vue.js test-project :) "/>
   </div>
 </template>
 
